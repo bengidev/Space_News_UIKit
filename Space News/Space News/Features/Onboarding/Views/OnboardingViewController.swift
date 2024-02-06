f48572f876e838590425bf600975fa4b9357d1f5
B e n g i <bambang.trd17@gmail.com>
2024-02-06 13:58:02 +0700

    chore: add Onboarding feature into Features

---
+++ Space News/Space News/Features/Onboarding/Views/OnboardingViewController.swift
@@ -0,0 +1,43 @@
+    //
+    //  OnboardingViewController.swift
+    //  Space News
+    //
+    //  Created by Bambang Tri Rahmat Doni on 06/02/24.
+    //
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+        // MARK: Initializers
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @available(*, unavailable)
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+
+        fatalError("awakeFromNib() has not been implemented")
+    }
+
+        // MARK: Lifecycles
+    override func loadView() {
+        super.loadView()
+
+        view.backgroundColor = .systemBlue
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            // Do any additional setup after loading the view.
+    }
+}
